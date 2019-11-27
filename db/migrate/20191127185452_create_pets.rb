3a4513f4edb4d95f19833c843b8e852401f42af1
Daniel <danmoran.pro@gmail.com>
2019-11-27 12:20:50 -0700

    Create pets model,view,controller, and testing for each.

---
+++ db/migrate/20191127185452_create_pets.rb
@@ -0,0 +1,13 @@
+class CreatePets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pets do |t|
+      t.string :image
+      t.string :name
+      t.integer :age
+      t.string :sex
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
